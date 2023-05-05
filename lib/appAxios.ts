1a6457e830d925cdb733a9f8f95fe629aad2f3e7
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-05-05 18:28:26 +0200

    change axios timeout to 10s

--- lib/appAxios.ts
+++ lib/appAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.BASE_URL,
-  timeout: 3000,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
