565f9dd1d9095ec6548aa66b415f9131dd9f8d67
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-05-10 06:58:35 +0200

    person who adds a review may see it before accepting by an admin

--- pages/api/reviews/get-all-revealed.ts
+++ pages/api/reviews/get-all-revealed.ts
@@ -1,6 +1,8 @@
 import dbConnect from '@/lib/dbConnect';
 import Review from '@/models/Review';
+import mongoose from 'mongoose';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getToken } from 'next-auth/jwt';
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,10 +13,22 @@ export default async function handler(
   }
 
   try {
+    const token = await getToken({ req });
+
     await dbConnect();
-    const reviews = await Review.find({ hidden: false })
-      .populate('author', 'username _id email imgUrl')
-      .sort({ createdAt: -1 });
+    let reviews = [];
+
+    if (token) {
+      reviews = await Review.find({
+        $or: [{ hidden: false }, { author: token._id }],
+      })
+        .populate('author', 'username _id email imgUrl')
+        .sort({ createdAt: -1 });
+    } else {
+      reviews = await Review.find({ hidden: false })
+        .populate('author', 'username _id email imgUrl')
+        .sort({ createdAt: -1 });
+    }
 
     return res.status(200).json({ reviews });
   } catch (err) {
