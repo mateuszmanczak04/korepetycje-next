ca1dbe9483a72cbbafe70e3e9a57d033cc880fe3
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-08-30 09:41:52 +0200

    add more info in Location

--- app/(components)/Location.tsx
+++ app/(components)/Location.tsx
@@ -4,10 +4,13 @@ const Location = () => {
       <h2>Lokalizacja</h2>
       <p className='mt-2'>
         Przyjmuję uczniów u siebie w mieszkaniu na{' '}
-        <span className='bg-primary-100'>Osiedlu Stefana Batorego</span> lub za
-        dodatkową opłatą przyjeżdżam do ucznia, jeśli mieszka w okolicy. Oferuję
-        również lekcje w formie <span className='bg-primary-100'>zdalnej</span>,
-        które najczęściej prowadzę przez discorda.
+        <span className='bg-primary-100'>
+          Osiedlu Stefana Batorego w Poznaniu
+        </span>{' '}
+        lub za dodatkową opłatą przyjeżdżam do ucznia, jeśli mieszka w okolicy.
+        Oferuję również lekcje w formie{' '}
+        <span className='bg-primary-100'>zdalnej</span>, które najczęściej
+        prowadzę przez discorda.
       </p>
     </section>
   );
