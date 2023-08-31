c9a18cd026c705c49f3e9715d2faf670b07c3266
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2023-08-31 09:23:20 +0200

    add open graph move favicon to metadata object

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,27 @@ import { AppContextProvider } from '@/context/AppContext';
 import { Metadata } from 'next';
 import { ReactNode } from 'react';
 import '../styles/globals.scss';
-import Footer from './(components)/Footer';
 
 export const metadata: Metadata = {
-  title: 'Korepetycje - Mateusz Mańczak',
+  title: 'Korepetycje z Matematyki - Mateusz Mańczak',
   description: 'Poznajmy się, abym mógł pomóc ci zrozumieć matematykę.',
+  openGraph: {
+    title: 'Korepetycje z Matematyki - Mateusz Mańczak',
+    url: 'https://korepetycje.mateuszmanczak.pl/',
+    description: 'Poznajmy się, abym mógł pomóc ci zrozumieć matematykę.',
+    images: [
+      {
+        url: 'https://korepetycje.mateuszmanczak.pl/images/og-image.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'pl_PL',
+    type: 'website',
+  },
+  icons: {
+    icon: '/images/favicon.ico',
+  },
 };
 
 type Props = {
@@ -16,9 +32,6 @@ type Props = {
 const RootLayout = ({ children }: Props) => {
   return (
     <html lang='pl'>
-      <head>
-        <link rel='icon' href='/images/favicon.ico' sizes='any' />
-      </head>
       <body>
         <AppContextProvider>{children}</AppContextProvider>
       </body>
